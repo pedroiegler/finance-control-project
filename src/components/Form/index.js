9a0e1a6d49818a38084d0c25dd4e5d17e4d0e90c
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-29 16:29:09 -0300

    fix: finalizando front-end

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -33,39 +33,24 @@ const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
 
   return (
     <>
-      <C.Container>
-        <C.InputContent>
-          <C.Label>Descrição</C.Label>
-          <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
-        </C.InputContent>
-        <C.InputContent>
-          <C.Label>Valor</C.Label>
-          <C.Input
-            value={amount}
-            type="number"
-            onChange={(e) => setAmount(e.target.value)}
-          />
-        </C.InputContent>
-        <C.RadioGroup>
-          <C.Input
-            type="radio"
-            id="rIncome"
-            defaultChecked
-            name="group1"
-            onChange={() => setExpense(!isExpense)}
-          />
-          <C.Label htmlFor="rIncome">Entrada</C.Label>
-          <C.Input
-            type="radio"
-            id="rExpenses"
-            name="group1"
-            onChange={() => setExpense(!isExpense)}
-          />
-          <C.Label htmlFor="rExpenses">Saída</C.Label>
-        </C.RadioGroup>
-        <C.Button onClick={handleSave}>ADICIONAR</C.Button>
-      </C.Container>
-      <Grid itens={transactionsList} setItens={setTransactionsList} />
+       <C.Container>
+                <C.InputContent>
+                    <C.Label>Descrição</C.Label>
+                    <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+                </C.InputContent>
+                <C.InputContent>
+                    <C.Label>Valor</C.Label>
+                    <C.Input value={amount} type="number" onChange={(e) => setAmount(e.target.value)} />
+                </C.InputContent>
+                <C.RadioGroup>
+                    <C.Input type="radio" id="rIncome" defaultChecked name="group1" onChange = {() => setExpense(!isExpense)} />
+                    <C.Label htmlFor="rIncome">Entrada</C.Label>
+                    <C.Input type="radio" id="rExpenses" name="group1" onChange = {() => setExpense(!isExpense)} />
+                    <C.Label htmlFor="rExpenses">Saída</C.Label>
+                </C.RadioGroup>
+                <C.Button onClick={handleSave}>ADICIONAR</C.Button>
+            </C.Container>
+            <Grid itens={transactionsList} setItens={setTransactionsList} />
     </>
   );
 };
